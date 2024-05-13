96289ebe1179c84286a82baf01b00e7a45779caa
Son <github@snry.me>
2024-05-13 19:29:52 -0400

    Fix yt-dlp output and path flags

--- lidarr/Audio.service.bash
+++ lidarr/Audio.service.bash
@@ -595,9 +595,9 @@ DownloadProcess () {
 
 		if [ "$2" == "YOUTUBE MUSIC" ]; then
 			if [ ${#1} -gt 11 ]; then
-				yt-dlp -i --sponsorblock-remove music_offtopic --xattrs --embed-metadata --no-embed-chapters --no-embed-info-json --format ba[ext=m4a] --yes-playlist -x --audio-quality 0 -o "$audioPath/incomplete/%(track_number)s - %(track)s.%(ext)s" "https://youtube.com/playlist?list=$1" 2>&1 | tee -a "/config/logs/$logFileName"
+				yt-dlp -i --sponsorblock-remove music_offtopic --xattrs --embed-metadata --no-embed-chapters --no-embed-info-json --format ba[ext=m4a] --yes-playlist -x --audio-quality 0 -P "$audioPath/incomplete" -o "%(track_number)s - %(track)s.%(ext)s" "https://youtube.com/playlist?list=$1" 2>&1 | tee -a "/config/logs/$logFileName"
 			else
-				yt-dlp -i --sponsorblock-remove music_offtopic --xattrs --embed-metadata --no-embed-chapters --no-embed-info-json --format ba[ext=m4a] -x --audio-quality 0 -o "$audioPath/incomplete/%(track_number)s - %(track)s.%(ext)s" "https://youtube.com/watch?v=$1" 2>&1 | tee -a "/config/logs/$logFileName"
+				yt-dlp -i --sponsorblock-remove music_offtopic --xattrs --embed-metadata --no-embed-chapters --no-embed-info-json --format ba[ext=m4a] -x --audio-quality 0 -P "$audioPath/incomplete" -o "%(track_number)s - %(track)s.%(ext)s" "https://youtube.com/watch?v=$1" 2>&1 | tee -a "/config/logs/$logFileName"
 			fi
 
 			# Verify Client Works...
